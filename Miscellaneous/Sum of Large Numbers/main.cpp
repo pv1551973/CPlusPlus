0277e3c9b9811fd900e419e2bebf906e97b005d4
Vinod Parameswaran <35186366+pv1551973@users.noreply.github.com>
2018-05-22 19:47:54 +1000

    Update main.cpp
    main.cpp implementing the test logic

--- Miscellaneous/Sum of Large Numbers/main.cpp
+++ Miscellaneous/Sum of Large Numbers/main.cpp
@@ -1,1 +1,17 @@
 
+#include "code.hpp"
+using std::cin;
+using std::endl;
+
+int main (){
+    
+    string num1, num2;
+    cout << "Enter the numbers (double values would be truncated):";
+    cin >> num1 >> num2;
+
+    addNums obj(num1,num2);
+
+    obj.print_sum();
+
+    return 0;
+}
