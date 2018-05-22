25c0d66c7a4a5715e952643e33423ff76ecd0f18
Vinod Parameswaran <35186366+pv1551973@users.noreply.github.com>
2018-05-22 19:32:47 +1000

    Update reverse_string.cpp
    code to reverse a string

--- String/Exercises/reverse_string.cpp
+++ String/Exercises/reverse_string.cpp
@@ -1,1 +1,38 @@
 
+#include<iostream>
+#include<string>
+
+using std::cin;
+using std::cout;
+using std::getline;
+using std::string;
+
+void reverse(string&);
+
+int main(){
+string s;
+cout << "Enter string:";
+//cin >> s;
+getline(cin,s);
+reverse(s);
+cout << "\n" << "The reversed string is " << s << "\n";
+return 0;
+}
+
+void reverse(string& s){
+char* p = const_cast<char*>(s.c_str());
+int i = 0;
+int j = s.length() - 1;
+do{
+auto temp = p[i];
+p[i] = p[j];
+p[j] = temp;
+//cout << "i " << i << " j " << j << "\n";
+//cout << "p[i] " << p[i] << " p[j] " << p[j] << "\n";
+//cout << "p " << p << "\n";
+i++;
+j--;
+}while(i <= j);
+s.assign(p);
+return;
+}
