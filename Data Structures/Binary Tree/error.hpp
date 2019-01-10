ceca1276636e5121ae1a5e85502a76e9e7be77d2
Vinod Parameswaran <35186366+pv1551973@users.noreply.github.com>
2019-01-10 18:25:07 +1100

    Add files via upload

--- Data Structures/Binary Tree/error.hpp
+++ Data Structures/Binary Tree/error.hpp
@@ -1,1 +1,20 @@
+#ifndef ERROR_HPP
+#define ERROR_HPP
+#include<type_traits>
 
+enum class error_code{BAD_NODE_POS};
+
+//function template to
+//convert an enumeration
+//class type to its corresponding
+//integral type for the sake of
+//printing
+
+template <typename Enumeration>
+auto as_integer(Enumeration const value)->typename std::underlying_type<Enumeration>::type{
+
+    return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+
+}
+
+#endif
