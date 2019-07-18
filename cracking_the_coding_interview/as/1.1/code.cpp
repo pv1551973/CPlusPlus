58af0581c276e382d7e2da39f7d3b9e979bdd381
Vinod Parameswaran <35186366+pv1551973@users.noreply.github.com>
2019-07-18 12:59:05 +1000

    Add files via upload

--- cracking_the_coding_interview/as/1.1/code.cpp
+++ cracking_the_coding_interview/as/1.1/code.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+#include<string>
 
+using std::string;
+using std::cout;
+using std::endl;
+
+string s = "abcdefghijklmnopqrstuvwxyz";
+
+int main (void){
+	auto start = s.begin();
+	auto end = s.end() - 1;
+	bool flag = true;
+
+	cout << s << endl;
+
+	do{
+		while(start != end){
+			if(start == end){
+				break;
+			}
+			if (*start == *end)
+			{
+				flag = false;
+				goto END;
+			}
+			++start;
+		}
+		start = s.begin();
+	}while(--end != s.begin());
+
+END:
+	if(flag){
+		cout << "Unique string" << endl;
+	}
+	else{
+		cout << "Not unique string" << endl;
+	}
+		return 0;
+}
