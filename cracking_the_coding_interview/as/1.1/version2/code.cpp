35e6d6b847729e7143778296c7c343e4e02f345c
Vinod Parameswaran <35186366+pv1551973@users.noreply.github.com>
2023-08-03 08:23:05 +1000

    Add files via upload

--- cracking_the_coding_interview/as/1.1/version2/code.cpp
+++ cracking_the_coding_interview/as/1.1/version2/code.cpp
@@ -1,1 +1,43 @@
+#ifndef CODE_H
+#define CODE_H
+#include "code.h"
+#endif
+
+int main (){
+ 
+ string input_string;
+
+ cout << "enter string: ";
+ cin >> input_string;
+ 
+ auto result = chk_if_uniq(input_string);
+ 
+ if(result){
+   cout << input_string << " contains unique characters." << endl; 
+ }
+else{
+   cout << input_string << " contains non-unique characters." << endl; 
+}
+
+return 0;
+}
+
+bool chk_if_uniq (string s){ 
+  
+  auto bIter = s.begin();
+  auto eIter = s.end();
+  bool iterPos = (bIter != eIter);
+  auto flag = true;
+
+  while(iterPos){
+   flag = per_scan(bIter,eIter);
+   if(!flag){
+     break;
+   }
+   advance(bIter,1);
+   iterPos = (bIter != eIter);
+  }
+
+ return flag;
+}
 
--- cracking_the_coding_interview/as/1.1/version2/code.h
+++ cracking_the_coding_interview/as/1.1/version2/code.h
@@ -1,1 +1,32 @@
+#include<string>
+#include<iterator>
+#include<iostream>
 
+using std::string;
+using std::advance;
+using std::iterator;
+using std::cin;
+using std::cout;
+using std::endl;
+
+bool chk_if_uniq (string); 
+
+template<typename iter>
+bool per_scan(iter it, iter eIter){
+
+  auto nxIt = it;
+  bool iterPos = (nxIt != eIter);
+  auto flag = true;
+
+  do{
+      ++nxIt;
+      iterPos = (nxIt != eIter);
+      if(iterPos){
+        if(*nxIt == *it){
+          flag = false;
+        }
+      }
+    }while(flag && iterPos);
+  
+ return flag;
+}
